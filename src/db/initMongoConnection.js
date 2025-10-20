4e522c03741f6703a70c572552bde2b54d56002b
PodletskyiDenys <denus.podletskiy@gmail.com>
2025-10-20 21:47:12 +0300

    my hw

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export const initMongoConnection = async () => {
+  try {
+    const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
+      process.env;
+
+    const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(uri);
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.error('Database connection error:', error.message);
+    process.exit(1);
+  }
+};
